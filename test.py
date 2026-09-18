47c59a124776de8b5490a80f39c43180c54fa0f7
Le Minh Khoi <10022007khoine@gmail.com>
2026-09-18 16:36:17 +0700

    mô tả thay đổi của bạn

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-pick= -1
+'''pick= -1
 while pick != 0:
     try:
         pick = int(input("Enter your choice function: "))
@@ -6,4 +6,9 @@ while pick != 0:
         print("(!) Invalid choice. Please try again.")
         continue
     break
-print(pick)
+print(pick)'''
+f=0.1
+if 2*f==0.200000000000001:
+    print("True")
+else:
+    print("False")
