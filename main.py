6436838475ebb105595520bcce3b3c2166089c06
Le Minh Khoi <10022007khoine@gmail.com>
2026-09-18 13:33:22 +0700

    update module

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 
 def main():
     pick= -1
+    a=[]
     while pick != 0:
         try:
             pick = int(input("Enter your choice function: "))
@@ -12,37 +13,35 @@ def main():
             if pick == 1:
                 #(1) Add Student
                 print("You have selected function 1")
-                name = str(input("Enter name student: "))
-                id = str(input("Enter id student: "))
-                birth = str(input("Enter birth student (YYYY/MM/DD): "))
-                phone = str(input("Enter phone student: "))
+                student_id = str(input("Enter id student: "))
+                full_name = str(input("Enter name student: "))
+                date_of_birth = str(input("Enter birth student (YYYY/MM/DD): "))
                 gender = str(input("Enter gender student (male/female): "))
                 class_name = str(input("Enter class student: "))
+                phone = str(input("Enter phone student: "))               
                 email = str(input("Enter email student: "))
-                status = str(input("Enter status student ( Active / Inactive / Graduated): "))
+                status = str(input("Enter status student ( active / inactive / graduated): "))
+                a.append((full_name, student_id, date_of_birth, gender, class_name, phone, email, status))
             elif pick == 2:
                 #(2) Add Course Student
                 print("You have selected function 2")
                 checkid= str(input("Enter id student: "))
-                if checkid == id:
-                    course_name = str(input("Enter course name: "))
+                if checkid == student_id:
                     course_id = str(input("Enter course id: "))
+                    course_name = str(input("Enter course name: "))
                     semester = str(input("Enter semester (EX: Fall2026): "))
                     credits = int(input("Enter credits: "))
                     grade_FE = input("Enter grade FE (ex: 6.7 or blank if not yet graded): ")
                     grade_RE = input("Enter grade PE (ex: 3.6 or blank if not yet graded): ")
-                    status = str(input("Enter status course (Pass/Fail): "))
+                    status_ok = str(input("Enter status course (Pass/Fail): "))
                 else:
                     print("(!) Student not found.")
             elif pick == 3:
                 #(3) Update Student
                 print("You have selected function 3")
                 checkid= str(input("Enter id student: "))
-                if checkid == id:
-                    name = str(input("Enter new name student: "))
-                    birth = str(input("Enter new birth student (YYYY/MM/DD): "))
+                if checkid == student_id:
                     phone = str(input("Enter new phone student: "))
-                    gender = str(input("Enter new gender student (male/female): "))
                     class_name = str(input("Enter new class student: "))
                     email = str(input("Enter new email student: "))
                     status = str(input("Enter new status student ( Active / Inactive / Graduated): "))
@@ -55,9 +54,11 @@ def main():
         except Exception:
             print("(!) Invalid input. Please try again.")
             continue
-    print(f"{name} has been added successfully.")
-    print(f"Student ID: {id}")
-    print(f"Birth: {birth}")
+    print(f"{full_name} has been added successfully.")
+    print(f"Student ID: {student_id}")
+    print(f"Birth: {date_of_birth}")
     print(f"Phone: {phone}")
+    print(a)
+
 if __name__ == "__main__":
     main()
