704a14fe52e999c9abfea193acfe1ed9e07f7492
letanphat3110-collab <letanphat3110@gmail.com>
2026-09-18 21:43:35 +0700

    mô tả thay đổi của bạn

--- result_manager.py
+++ result_manager.py
@@ -1,12 +1,27 @@
 from Result import Result #Import Result class
-#from File_Hanlder import FileHandler, FileIOException
+from File_helper import FileHelper, FileIOException #Import FileHelper and related Exception
+# ==============================================================================
+# Custom Exception
+# ==============================================================================
+
+class RESULTManagerException(Exception):
+    """
+    Custom Exception for business logic errors.
+    """
+    pass
+#End class
+
+# ==============================================================================
+# ResultManager Class
+# ==============================================================================
 class ResultManager:
     def __init__(self):
         #Khởi tạo danh sách kết quả 
+        self.file_helper= FileHelper
         self._result: list[Result]= []
     def add_result(self, new_result: Result):
         """
-        ======1. Add a new result to system======
+        ==1. Add a new result to system==
         """
         #Check if student_id and course_id already exists
         for r in self._result:
@@ -19,7 +34,7 @@ class ResultManager:
     #End def
     def update_result(self, student_id:str, course_id:str, grade_fe: float=None,grade_re: float=None)->bool:
         """
-        ======2. Update FE or RE grade for a Student======
+        ==2. Update FE or RE grade for a Student==
         """
         for r in self._result:
             if r.student_id == student_id and r.course_id == course_id:
@@ -34,7 +49,7 @@ class ResultManager:
     #End def 
     def search_student_id(self, student_id: str)->list[Result]:
         """
-        ======3. Search/Display enrollment by Student ID======
+        ==3. Search/Display enrollment by Student ID==
         """
         found_result = []
         for r in self._result:
@@ -42,4 +57,38 @@ class ResultManager:
                 found_result.append(r)
         return found_result
     def get_sorted_by_status(self)->list[Result]:
-        
+        """
+        ==4.Sort exam results list by Status descending==
+        """
+        #Create list containing passed subjects and list containing failed subjects
+        pass_list= []
+        fail_list= []
+        #Review each result to categorize it.
+        for r in self._result:
+            if r.get_status() == "Pass":
+                pass_list.append(r)
+            else:
+                fail_list.append(r)
+        #Concatenate the two lists (Pass first, Fail second).
+        sorted_result= pass_list + fail_list
+        return sorted_result 
+    #End def 
+    def display_passed_student(self):
+        """
+        ==5. Display the list of students/results with a "Pass" status==
+        """
+        #Create a list containing passed subjects
+        passed_list= []
+        #Review each result (r) in overal list
+        for r in self._result:
+            if r.get_status() == "Pass":
+            #If it is indeed a pass, add it to the list.
+                passed_list.append(r)
+            #End if 
+        if len(passed_list)==0:
+            print("No student/results have achieved a 'Pass' yet!")
+            return
+        #End if 
+        print("\n--LIST SORTED BY STATUS (PASSED FIRST)")
+        for r in passed_list:
+            print(r)
